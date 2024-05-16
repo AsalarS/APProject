5c070f090796dfdbea0a680b9ae349068b8b02a6
Hassan <63836815+Hassan-jaffer@users.noreply.github.com>
2024-05-16 13:48:06 +0300

    aaa

--- adminApp/Pages/categoryPage.Designer.cs
+++ adminApp/Pages/categoryPage.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(334, 178);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(588, 151);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.TabIndex = 1;
             // 
             // categoryPage
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(0, 0, 24);
-            ClientSize = new Size(858, 611);
+            ClientSize = new Size(1226, 1018);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
             Name = "categoryPage";
             Text = "categoryPage";
+            Load += categoryPage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
     }
 }
--- adminApp/Pages/categoryPage.cs
+++ adminApp/Pages/categoryPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeCareObjects.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace AdminApp.Pages
 {
     public partial class categoryPage : Form
     {
+        HomeCareDBContext context;
         public categoryPage()
         {
             InitializeComponent();
+            context = new HomeCareDBContext();
+        }
+
+        private void categoryPage_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = context.Users.ToList();
+            
         }
     }
 }
