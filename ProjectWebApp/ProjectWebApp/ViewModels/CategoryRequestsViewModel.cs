868778df4187f2c53f379aee64909a8bc0ca05e1
Hasoooni2003 <Hasan.alshehabi2003@gmail.com>
2024-05-28 23:20:16 +0300

    added dashboard

---
+++ ProjectWebApp/ProjectWebApp/ViewModels/CategoryRequestsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HomeCareObjects.Model;
+
+namespace HomeCareWebApp.ViewModels
+{
+    public class CategoryRequestsViewModel
+    {
+        public int ActiveRequests { get; set; }
+        public int PendingRequests { get; set; }
+        public int CompletedRequests { get; set; }
+        public int OverdueRequests { get; set; }
+        public Category Category { get; set; }
+    }
+}
